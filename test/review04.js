af0154fe8aaced46923f9ddfe1ea1830c4dc652f
rikiya <m0a20010c0@teu.edu.ac.jp>
2021-11-07 17:42:32 +0900

    text

--- test/review04.js
+++ test/review04.js
@@ -1,7 +1,29 @@
-function hello(){}
-hello("こんにちはこんにちは");
+function hello(){
+  console.log("こんにちはこんにちは");
+}
+hello();
 
 function hellosomeone(name){
   console.log(`こんにちは${name}さん`);
 }
-hellosomeone("ゴルドーロジャー");
+hellosomeone("引数1");
+
+function helloCircleArea(radius){
+  console.log(radius * radius * 3.14);
+}
+helloCircleArea(3);
+
+function helloRectangleArea(width, height){
+  console.log(width * height);
+}
+helloRectangleArea(2,5);
+
+function circleArea(radius){
+  return radius * radius * 3.14;
+}
+console.log(`circleArea(3):${circleArea(3)}`);
+
+function rectangleArea(width, height){
+  return width * height;
+}
+console.log(`rectangleArea(2, 5):${rectangleArea(2, 5)}`);
