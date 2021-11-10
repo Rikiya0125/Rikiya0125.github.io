8dff9faad3d394661d50bef30a4ea302e442b283
rikiya <m0a20010c0@teu.edu.ac.jp>
2021-11-10 14:46:53 +0900

    text

---
+++ test/modal4.js
@@ -0,0 +1,16 @@
+var $countA = 0;
+
+function plusOne(){
+  let modal = document.querySelector("#modal");
+  modal.innerHTML = ++$countA;
+}
+
+function plusFive(){
+  let modal = document.querySelector("#modal");
+
+}
+
+function plusTen(){
+  let modal = document.querySelector("#modal");
+  modal.
+}
