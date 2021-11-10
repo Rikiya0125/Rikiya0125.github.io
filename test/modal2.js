95617d969ce259992a69f6bd4bd0779ab192e728
rikiya <m0a20010c0@teu.edu.ac.jp>
2021-11-10 14:07:46 +0900

    text

---
+++ test/modal2.js
@@ -0,0 +1,14 @@
+function changeRed(){
+  let modal = document.querySelector("#modal");
+  modal.style.backgroundColor = "red";
+}
+
+function changeYellow(){
+  let modal = document.querySelector("#modal");
+  modal.style.backgroundColor = "yellow";
+}
+
+function changeGreen(){
+  let modal = document.querySelector("#modal");
+  modal.style.backgroundColor = "green";
+}
---
+++ test/modal3.js
@@ -0,0 +1,9 @@
+function appendCircle(){
+  let modal = document.querySelector("#modal");
+  modal.insertAdjacentHTML("beforeend", "◯");
+}
+
+function appendCross(){
+  let modal = document.querySelector("#modal");
+  modal.insertAdjacentHTML("beforeend", "✕");
+}
